a545972a5664b211cd219fd10d5eac1289d6d167
José Eustaquio Muniz Santos <eustaquio.cadastros@gmail.com>
2022-11-01 20:44:02 -0300

    mobile environment created

---
+++ mobile/src/screens/Signin.tsx
@@ -0,0 +1,39 @@
+import { Text, Center } from "native-base";
+
+export function SignIn() {
+  {
+    /* <View style={styles.container}> OU
+      <VStack
+    flex={1}
+    bgColor="fuchsia.400"
+    alignItems="center"
+    justifyContent="center"
+  > OU
+  <Center />
+  */
+  }
+  return (
+    <Center flex={1} bgColor="gray.900">
+      <Text color="white" fontSize={24} fontFamily="heading">
+        SignIn
+      </Text>
+    </Center>
+  );
+}
+
+{
+  /*
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#000000",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  title: {
+    color: "#ffffff",
+    fontSize: 24,
+  },
+});
+ */
+}
